3572e0d60781d4f2272d4bf26a98f00c37a10190
monu-19 <111163702+monu-19@users.noreply.github.com>
2022-08-20 19:24:05 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,7 +6,3 @@ def newMaven()
 {
   sh 'mvn package'
 }
-def newDeploy(ip,appname)
-{
-  deploy adapters: [tomcat9(credentialsId: '4a198297-7196-45d6-9475-249fcb77e483', path: '', url:"$(ip)"], contextPath:"$(appname)", war: '**/*.war'
-}
